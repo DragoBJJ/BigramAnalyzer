9a0dc71a8290f4e8f0b8acdd1f496706d2364c7f
jakub <j.pawelski.it@gmail.com>
2024-01-20 21:32:15 +0100

    Deleted description and added it ot README.md

--- BigramAnalyzer/BigramAnalyzer.cs
+++ BigramAnalyzer/BigramAnalyzer.cs
@@ -6,14 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-/* REQUIREMENTS - GetCount method should be implemented in an optimal way, due to its potential frequent use.
- * 
- ANSWER - creating a new instance of a dictionary is likely to be more performance heavy than cleaning the dictionary. 
-        This is because creating a new instance involves memory allocation and initialization,
-         which can be relatively costly in terms of performance. On the other hand,
-        cleaning the dictionary typically involves removing existing elements,
-        which may be a more lightweight operation.
-*/
+ 
 
 namespace Bigram
 {
